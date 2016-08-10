a15b21a2d7db58442da24f130fb71b7d365e7e28
pverspeelt <phiver4@gmail.com>
2016-08-10 17:09:12 +0200

    Added additional material and started with extra code

--- Code for chapter2.R
+++ Code for chapter2.R
@@ -10,3 +10,61 @@ head(digits_train[, 1:4])
 ## convert to factor
 digits_train$label <- factor(digits_train$label, levels = 0:9)
 
+# reduce the number of observations for the sake of speed.
+i <- 1:5000
+digits_x <- digits_train[i, -1]
+#define digits_y with the dollar sign not with [i, 1]. Tibbles keep their structure unlike data.frame
+digits_y <- digits_train$label[i]
+
+# check the distribution
+barplot(table(digits_y))
+
+
+library(caret)
+library(nnet)
+
+# start out with 5 hidden neurons
+# maximum number of weights at 10000
+
+set.seed(1234)
+digits_model1 <- train(x = digits_x, 
+                       y = digits_y,
+                       method = "nnet",
+                       tuneGrid = expand.grid(
+                         .size = c(5),
+                         .decay = 0.1),
+                       trControl = trainControl(method = "none"),
+                       MaxNWts = 10000,
+                       maxit = 100)
+
+pred_model1 <- predict(digits_model1)
+barplot(table(pred_model1))
+
+confusionMatrix(xtabs(~pred_model1 + digits_y))
+# accuracy of 44.32%
+
+
+# increasing from 5 to 10 hidden neurons
+# increasing maximum number of weights from 10000 to 50000
+set.seed(1234)
+starttime <- Sys.time()
+digits_model2 <- train(x = digits_x, 
+                       y = digits_y,
+                       method = "nnet",
+                       tuneGrid = expand.grid(
+                         .size = c(10),
+                         .decay = 0.1),
+                       trControl = trainControl(method = "none"),
+                       MaxNWts = 50000,
+                       maxit = 100)
+endtime <- Sys.time()
+endtime - starttime
+# 2.261349 mins
+
+
+pred_model2 <- predict(digits_model2)
+barplot(table(pred_model2))
+
+confusionMatrix(xtabs(~pred_model2 + digits_y))
+# accuracy of 57.64%
+
--- README.md
+++ README.md
@@ -4,3 +4,15 @@ This repository contains the code used in the book R Deep Learning Essentials. T
 
 This was the first time I saw an author using the checkpoint package to make sure the code is reproducible. More information about the checkpoint package can be found on [cran](https://cran.r-project.org/web/packages/checkpoint/index.html).
 
+
+
+Some additional material on deep learning can be found by the following links:
+
+Deep learning for computational biology ([link](http://classic.msb.embopress.org/content/12/7/878))
+Tensorflow resources ([link](https://hackerlists.com/tensorflow-resources/))
+[This question](http://stats.stackexchange.com/questions/226911/neural-network-references-textbooks-online-courses-for-beginners#226919) on cross validated about Neural network references for beginners
+
+[H2O](http://www.h2o.ai/)
+[H2O documentation](https://h2o-release.s3.amazonaws.com/h2o/rel-turing/3/docs-website/index.html)
+[mxnet](https://github.com/dmlc/mxnet)
+
