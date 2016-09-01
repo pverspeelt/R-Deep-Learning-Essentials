0bb66229a21adb1dc53381edfce52ca63458f26d
pverspeelt <phiver4@gmail.com>
2016-09-01 11:56:19 +0200

    added models m2a, m2b and m2c

--- Code for chapter4.R
+++ Code for chapter4.R
@@ -38,11 +38,12 @@ xnames <- colnames(h2o_digits_train)
 # This is different from book
 
 
-
+starttime <- Sys.time()
 m1 <- h2o.deeplearning(
   x = xnames,
   training_frame= h2o_digits_train,
   validation_frame = h2o_digits_test,
+  model_id = "m1",
   activation = "TanhWithDropout",
   autoencoder = TRUE,
   hidden = c(50),
@@ -53,6 +54,90 @@ m1 <- h2o.deeplearning(
   l1 = 0,
   l2 = 0
 )
+endtime <- Sys.time()
+endtime - starttime
+#  4.541138 mins
 
+
+# start with a single layer
+# 100 hidden neurons
+# 20 training iterations => epochs
+# no regularization
+
+starttime <- Sys.time()
+m2a <- h2o.deeplearning(
+  x = xnames,
+  training_frame= h2o_digits_train,
+  validation_frame = h2o_digits_test,
+  model_id = "m2a",
+  activation = "TanhWithDropout",
+  autoencoder = TRUE,
+  hidden = c(100),
+  epochs = 20,
+  sparsity_beta = 0,
+  input_dropout_ratio = 0,
+  hidden_dropout_ratios = c(0),
+  l1 = 0,
+  l2 = 0
+)
+endtime <- Sys.time()
+endtime - starttime
+#  4.069617 mins
+
+# start with a single layer
+# 100 hidden neurons
+# 20 training iterations => epochs
+# sparsity_beta = 0.5
+
+starttime <- Sys.time()
+m2b <- h2o.deeplearning(
+  x = xnames,
+  training_frame= h2o_digits_train,
+  validation_frame = h2o_digits_test,
+  model_id = "m2b",
+  activation = "TanhWithDropout",
+  autoencoder = TRUE,
+  hidden = c(100),
+  epochs = 20,
+  sparsity_beta = 0.5,
+  input_dropout_ratio = 0,
+  hidden_dropout_ratios = c(0),
+  l1 = 0,
+  l2 = 0
+)
+endtime <- Sys.time()
+endtime - starttime
+#  4.741505 mins
+
+
+# start with a single layer
+# 50 hidden neurons
+# 20 training iterations => epochs
+# input_dropout_ratio = 0.2
+
+starttime <- Sys.time()
+m2c <- h2o.deeplearning(
+  x = xnames,
+  training_frame= h2o_digits_train,
+  validation_frame = h2o_digits_test,
+  model_id = "m2c",
+  activation = "TanhWithDropout",
+  autoencoder = TRUE,
+  hidden = c(100),
+  epochs = 20,
+  sparsity_beta = 0,
+  input_dropout_ratio = 0.2,
+  hidden_dropout_ratios = c(0),
+  l1 = 0,
+  l2 = 0
+)
+endtime <- Sys.time()
+endtime - starttime
+#  2.042921 mins
+
+m1
+m2a
+m2b
+m2c
 
 h2o.shutdown(prompt = FALSE)
