29d58db97f6fb7088fcada21b7ccb8013fac5f9e
pverspeelt <phiver4@gmail.com>
2016-10-11 20:06:21 +0200

    Finished chapter 6

--- Code for chapter6.R
+++ Code for chapter6.R
@@ -84,6 +84,10 @@ grid.arrange(
 # take a seed and then randomly sample a number of hyperparameters, 
 # store the sampled parameters, run the model, and return the results.
 
+# when using dropout for regularization, it is common to have a relatively 
+# smaller amount of dropout for the input variables (around 20% commonly) 
+# and a higher amount for hidden neurons (around 50% commonly).
+
 
 par(mfrow = c(2, 1))
 plot(
@@ -98,10 +102,240 @@ plot(
   type = "l", xlab = "x", ylab = "Density",
   main = "Density of a beta(1.5, 1) / 2")
 
-# TODO
+
+# load HAR dataset ------
+har_train_x <- readr::read_table("Data/UCI HAR Dataset/train/X_train.txt", col_names = FALSE)
+har_train_y <- readr::read_table("Data/UCI HAR Dataset/train/y_train.txt", col_names = FALSE)
+# unlist readr reads in _y as a list and doesn't work with factor
+har_train <- cbind(har_train_x, outcome = factor(unlist(har_train_y)))
+
+har_test_x <- readr::read_table("Data/UCI HAR Dataset/test/X_test.txt", col_names = FALSE)
+har_test_y <- readr::read_table("Data/UCI HAR Dataset/test/y_test.txt", col_names = FALSE)
+# unlist readr reads in _y as a list and doesn't work with factor
+har_test <- cbind(har_test_x, outcome = factor(unlist(har_test_y)))
+
+har_labels <- readr::read_table("Data/UCI HAR Dataset/activity_labels.txt", col_names = FALSE)
+
+h2oactivity_train <- as.h2o(
+  har_train,
+  destination_frame = "h2oactivitytrain")
+
+h2oactivity_test <- as.h2o(
+  har_test,
+  destination_frame = "h2oactivitytest")
+
+
+# If rho = 1, then the current gradient is not used and it is completely based
+# on the previous gradients. If rho = 0, the previous gradients are 
+# not used and it is completely based on the current gradient. 
+# Typically, values between .9 and .999 are used.
+
+# The epsilon parameter is a small constant that is added when taking 
+# the root mean square of previous squared gradients to improve conditioning 
+# (it is ideal to avoid this becoming actually zero) 
+# and is typically a very small number
+
+# H2O uses ADADELTA (Zeiler, M. D. (2012)) algorithm to automatically tune the learning rate
+
+# sample the depth or number of layers from 1 to 5 
+# sample the number of neurons in each layer from 20 to 600
+# by default each will have an equal probability.
+
+run <- function(seed, name = paste0("m_", seed), run = TRUE) {
+  set.seed(seed)
+  
+  p <- list(
+    name = name,
+    seed = seed,
+    depth = sample(1:5, 1),
+    l1 = runif(1, 0, .01),
+    l2 = runif(1, 0, .01),
+    input_dropout = rbeta(1, 1, 12),
+    rho = runif(1, .9, .999),
+    epsilon = runif(1, 1e-10, 1e-4))
+  
+  p$neurons <- sample(20:600, p$depth, TRUE)
+  p$hidden_dropout <- rbeta(p$depth, 1.5, 1)/2
+  
+  if (run) {
+    model <- h2o.deeplearning(
+      x = colnames(har_train_x),
+      y = "outcome",
+      training_frame = h2oactivity_train,
+      activation = "RectifierWithDropout",
+      hidden = p$neurons,
+      epochs = 100,
+      loss = "CrossEntropy",
+      input_dropout_ratio = p$input_dropout,
+      hidden_dropout_ratios = p$hidden_dropout,
+      l1 = p$l1,
+      l2 = p$l2,
+      rho = p$rho,
+      epsilon = p$epsilon,
+      export_weights_and_biases = TRUE,
+      model_id = p$Name
+    )
+    
+    ## performance on training data
+    p$MSE <- h2o.mse(model)
+    p$R2 <- h2o.r2(model)
+    p$Logloss <- h2o.logloss(model)
+    p$CM <- h2o.confusionMatrix(model)
+    
+    ## performance on testing data
+    perf <- h2o.performance(model, h2oactivity_test)
+    p$T.MSE <- h2o.mse(perf)
+    p$T.R2 <- h2o.r2(perf)
+    p$T.Logloss <- h2o.logloss(perf)
+    p$T.CM <- h2o.confusionMatrix(perf)
+    
+  } else {
+    model <- NULL
+  }
+  
+  return(list(
+    Params = p,
+    Model = model))
+}
+
+# to make the parameters reproducible
+use_seeds <- c(403L, 10L, 329737957L, -753102721L, 1148078598L, -1945176688L,
+               -1395587021L, -1662228527L, 367521152L, 217718878L, 1370247081L,
+               571790939L, -2065569174L, 1584125708L, 1987682639L, 818264581L,
+               1748945084L, 264331666L, 1408989837L, 2010310855L, 1080941998L,
+               1107560456L, -1697965045L, 1540094185L, 1807685560L, 2015326310L,
+               -1685044991L, 1348376467L, -1013192638L, -757809164L, 1815878135L,
+               -1183855123L, -91578748L, -1942404950L, -846262763L, -497569105L,
+               -1489909578L, 1992656608L, -778110429L, -313088703L, -758818768L,
+               -696909234L, 673359545L, 1084007115L, -1140731014L, -877493636L,
+               -1319881025L, 3030933L, -154241108L, -1831664254L)
+
+starttime <- Sys.time()
+model_results <- lapply(use_seeds, run)
+endtime <- Sys.time()
+endtime - starttime
+# 1.542883 hours
+
+model_results_dat <- do.call(rbind, 
+                             lapply(model_results, 
+                                    function(x) with(x$Params,
+                                                     data.frame(l1 = l1, 
+                                                                l2 = l2,
+                                                                depth = depth, 
+                                                                input_dropout = input_dropout,
+                                                                SumNeurons = sum(neurons),
+                                                                MeanHiddenDropout = mean(hidden_dropout),
+                                                                rho = rho, 
+                                                                epsilon = epsilon, 
+                                                                MSE = T.MSE))))
+
+
+plot_performance <- ggplot(data.table::melt(model_results_dat, id.vars = c("MSE")), aes(value, MSE)) +
+  geom_point() +
+  stat_smooth(color = "black") +
+  facet_wrap(~ variable, scales = "free_x", ncol = 2) +
+  theme_classic()
+
+plot_performance
+
+
+# In addition to viewing the univariate relations between parameters and
+# the model error, it can be helpful to use a multivariate model to 
+# simultaneously take different parameters into account.
+
+# To fit this (and allow some non-linearity), you can use a generalized 
+# additive model, using the gam() function from the mgcv package. 
+# We specifically hypothesize an interaction between the model depth 
+# and total number of hidden neurons, which we capture by including both
+# of those terms in a tensor expansion using the te() function, 
+# with the remaining terms given univariate smooths, using the s() function. 
+library(mgcv)
+m_gam <- gam(MSE ~ s(l1, k = 4) +
+               s(l2, k = 4) +
+               s(input_dropout) +
+               s(rho, k = 4) +
+               s(epsilon, k = 4) +
+               s(MeanHiddenDropout, k = 4) +
+               te(depth, SumNeurons, k = 4),
+             data = model_results_dat)
+
+summary(m_gam)
+
+par(mfrow = c(3, 2))
+for (i in 1:6) {
+  plot(m_gam, select = i)
+}
+
+par(mfrow = c(1, 1))
+plot(m_gam, select = 7)
+# In general, it seems that, the more layers there are, 
+# the more neurons are required to achieve a comparable performance:
+
+
+# run optimized model ------
+model_optimized <- h2o.deeplearning(
+  x = colnames(har_train_x),
+  y = "outcome",
+  training_frame = h2oactivity_train,
+  activation = "RectifierWithDropout",
+  hidden = c(300, 300, 300),
+  epochs = 100,
+  loss = "CrossEntropy",
+  input_dropout_ratio = .08,
+  hidden_dropout_ratios = c(.50, .50, .50),
+  l1 = .002,
+  l2 = 0,
+  rho = .95,
+  epsilon = 1e-10,
+  export_weights_and_biases = TRUE,
+  model_id = "optimized_model"
+)
+
+
+h2o.performance(model_optimized, h2oactivity_test)
+# H2OMultinomialMetrics: deeplearning
+# 
+# Test Set Metrics: 
+#   =====================
+#   
+#   MSE: (Extract with `h2o.mse`) 0.04552533
+# RMSE: (Extract with `h2o.rmse`) 0.2133667
+# Logloss: (Extract with `h2o.logloss`) 0.1566842
+# Mean Per-Class Error: 0.06015901
+# Confusion Matrix: Extract with `h2o.confusionMatrix(<model>, <data>)`)
+# =========================================================================
+#   Confusion Matrix: vertical: actual; across: predicted
+# 1   2   3   4   5   6  Error          Rate
+# 1      482   1  13   0   0   0 0.0282 =    14 / 496
+# 2       12 445  13   0   1   0 0.0552 =    26 / 471
+# 3        4  22 394   0   0   0 0.0619 =    26 / 420
+# 4        0   2   0 445  43   1 0.0937 =    46 / 491
+# 5        0   0   0  51 481   0 0.0959 =    51 / 532
+# 6        0   0   0   0  14 523 0.0261 =    14 / 537
+# Totals 498 470 420 496 539 524 0.0601 = 177 / 2.947
+# 
+# Hit Ratio Table: Extract with `h2o.hit_ratio_table(<model>, <data>)`
+# =======================================================================
+#   Top-6 Hit Ratios: 
+#   k hit_ratio
+# 1 1  0.939939
+# 2 2  0.994571
+# 3 3  0.999661
+# 4 4  0.999661
+# 5 5  1.000000
+# 6 6  1.000000
+
+
+model_results_dat[which.min(model_results_dat$MSE), ]
+# l1           l2 depth input_dropout SumNeurons MeanHiddenDropout       rho      epsilon        MSE
+# 18 0.002379311 0.0001135312     5  9.989629e-05       2186         0.3903965 0.9648398 2.993998e-06 0.06072076
+
+# it is also possible to optimize hyperparameters more formally, 
+# such as using Bayesian optimazation of hyperparameters
+
 
 # stop h2o cluster
 h2o.shutdown(prompt = FALSE)
 
 
-h2o_dmeanimp[, v][is.na(h2o_dmeanimp[, v])]
+
